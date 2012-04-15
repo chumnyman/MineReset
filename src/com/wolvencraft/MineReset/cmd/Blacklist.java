0e72e39bd1a02c19ee3f2da6004868176f99dc3c
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-04-15 07:38:27 -0700

    Added blacklist

--- src/com/wolvencraft/MineReset/cmd/Blacklist.java
+++ src/com/wolvencraft/MineReset/cmd/Blacklist.java
@@ -1,22 +1,99 @@
 package com.wolvencraft.MineReset.cmd;
 
-import org.bukkit.command.CommandSender;
-
-import com.wolvencraft.MineReset.CommandManager;
-import com.wolvencraft.MineReset.MineReset;
+import java.util.List;
 
 public class Blacklist
 {
-	public static void run()
+	public static void run(String[] args)
 	{
-		CommandSender sender = CommandManager.getSender();
-		MineReset plugin = CommandManager.getPlugin();
+		if(args.length == 1)
+		{
+			Help.getBlacklist();
+		}
+		else if(args.length > 4 || args.length == 2)
+		{
+			Util.sendInvalid(args);
+			return;
+		}
 		
-		// This function should allow the user to enable blacklist / whitelist for each
-		// individual mine, as well as add and remove blocks from them. Checks for the
-		// correctness of arguments should be included.
+		String mineName = "";
+		if(!Util.mineExists(args[1]))
+		{
+			Util.sendError("Mine " + args[1] + " does not exist");
+			return;
+		}
+		else mineName = args[1];
 		
-		// Help.java includes command help. Util.java has a bunch of helpful functions
-		// for accessing the configuration and printing out messages to the user.
+		if(args[2].equalsIgnoreCase("toggle"))
+		{
+			if(Util.getRegionBoolean("mines." + mineName + ".blacklist.enabled"))
+			{
+				Util.setRegionBoolean("mines." + mineName + ".blacklist.enabled", false);
+				Util.sendSuccess("Blacklist turned OFF for mine " + mineName);
+			}
+			else
+			{
+				Util.setRegionBoolean("mines." + mineName + ".blacklist.enabled", true);
+				Util.sendSuccess("Blacklist turned ON for mine " + mineName);
+			}
+			return;
+		}
+		else if(args[2].equalsIgnoreCase("whitelist"))
+		{
+			if(Util.getConfigBoolean("mines." + mineName + ".blacklist.whitelist"))
+			{
+				Util.setRegionBoolean("mines." + mineName + ".blacklist.whitelist", false);
+				Util.sendSuccess("Blacklist is no longer treated as a whitelist for mine " + mineName);
+			}
+			else
+			{
+				Util.setRegionBoolean("mines." + mineName + ".blacklist.whitelist", true);
+				Util.sendSuccess("Blacklist is now treated as a whitelist for mine " + mineName);
+			}
+			return;
+		}
+		else if(args[2].equalsIgnoreCase("add"))
+		{
+			int blockID = Util.getBlockId(args[3]);
+			if(blockID == -1)
+			{
+				Util.sendError("Block '"+ args[3] + "' does not exist");
+				return;
+			}
+			
+			List<String> blacklist = Util.getRegionList("mines." + mineName + ".blacklist.blocks");
+			blacklist.add("" + blockID);
+			Util.setRegionList("mines." + mineName + ".blacklist.blocks", blacklist);
+
+			Util.sendSuccess("Block " + args[2] + " is now in the blacklist");
+			return;
+		}
+		else if(args[2].equalsIgnoreCase("remove"))
+		{
+			int blockID = Util.getBlockId(args[3]);
+			if(blockID == -1)
+			{
+				Util.sendError("Block '"+ args[3] + "' does not exist");
+				return;
+			}
+			
+			List<String> blacklist = Util.getRegionList("mines." + mineName + ".blacklist.blocks");
+			
+			int index = blacklist.indexOf(blockID);
+			if(index == -1)
+			{
+				Util.sendError("Block " + args[2] + " is not in the blacklist");
+				return;
+			}
+			blacklist.remove(index);
+			Util.setRegionList("mines." + mineName + ".blacklist.blocks", blacklist);
+			Util.sendSuccess("Block " + args[2] + " has been removed from the blacklist");
+			return;
+		}
+		else
+		{
+			Util.sendInvalid(args);
+			return;
+		}
 	}
 }

313b12d69ae887a31554f76ddd4c9a0aa91d1fb7
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-06-07 14:04:49 -0700

    Implement SignClass and a corresponding SignUtils

---
+++ src/com/wolvencraft/MineReset/mine/SignClass.java
@@ -0,0 +1,97 @@
+package com.wolvencraft.MineReset.mine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Sign;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
+
+@SerializableAs("Sign")
+public class SignClass implements ConfigurationSerializable, Listener  {
+	private World world;
+	private Location loc;
+	private Mine parent;
+	private boolean reset;
+	private List<String> lines;
+	
+	public SignClass(Mine parent, Location loc, Sign sign) {
+		this.parent = parent;
+		world = loc.getWorld();
+		this.loc = loc;
+		for(int i = 0; i < sign.getLines().length; i++) {
+			lines.add(sign.getLine(i));
+		}
+	}
+	
+	public SignClass(Map<String, Object> me) {
+        world = Bukkit.getWorld((String) me.get("world"));
+        loc = ((Vector) me.get("loc")).toLocation(world);
+        parent = (Mine) me.get("parent");
+        reset = (Boolean) me.get("reset");
+        lines = (List<String>) me.get("lines");
+	}
+	
+    public Map<String, Object> serialize() {
+        Map<String, Object> me = new HashMap<String, Object>();
+        me.put("loc", loc.toVector());
+        me.put("world", world.getName());
+        me.put("parent", parent);
+        me.put("reset", reset);
+        return me;
+    }
+    
+    /**
+     * Returns the location of the sign
+     * @return The sign location
+     */
+    public Location getLocation() {
+    	return loc;
+    }
+    
+    /**
+     * Returns the parent mine of the specific sign
+     * @return The parent mine
+     */
+    public Mine getParent() {
+    	return parent;
+    }
+    
+    /**
+     * Checks if a sign should reset its parent mine on right-click
+     * @return true if reset is enabled, false otherwise
+     */
+    public boolean getReset() {
+    	return reset;
+    }
+    
+    /**
+     * Gets the unmodified lines for the signs
+     * @return the original text on the sign
+     */
+    public List<String> getLines() {
+    	return lines;
+    }
+    
+    /**
+     * Changes the parent mine of the sign
+     * @param parent New parent
+     */
+    public void setParent(Mine parent) {
+    	this.parent = parent;
+    }
+    
+    /**
+     * Toggles whether a sign should reset a mine or not
+     * @param reset New value for the reset
+     */
+    public void setReset(boolean reset) {
+    	this.reset = reset;
+    }
+}
---
+++ src/com/wolvencraft/MineReset/util/SignUtils.java
@@ -0,0 +1,19 @@
+package com.wolvencraft.MineReset.util;
+
+import org.bukkit.Location;
+
+import com.wolvencraft.MineReset.mine.SignClass;
+
+public class SignUtils {
+	public static boolean exists(Location loc)
+	{
+		//TODO Stub
+		return true;
+	}
+	
+	public static SignClass getSign(Location loc)
+	{
+		//TODO Stub
+		return null;
+	}
+}

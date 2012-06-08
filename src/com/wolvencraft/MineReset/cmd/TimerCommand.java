e2bb446472f1b3e6beab4f2a79b3aed56424ea07
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-06-07 18:52:56 -0700

    Change some more commands to new format

--- src/com/wolvencraft/MineReset/cmd/TimerCommand.java
+++ src/com/wolvencraft/MineReset/cmd/TimerCommand.java
@@ -4,136 +4,133 @@ import java.util.List;
 
 import com.wolvencraft.MineReset.CommandManager;
 import com.wolvencraft.MineReset.config.Language;
-import com.wolvencraft.MineReset.config.Regions;
 import com.wolvencraft.MineReset.mine.Mine;
 import com.wolvencraft.MineReset.util.Message;
 import com.wolvencraft.MineReset.util.Util;
 
 
-public class TimerCommand
-{
-	public static void run(String[] args)
-	{
-		if(!Util.senderHasPermission("edit"))
-		{
+public class TimerCommand {
+	
+	public static void run(String[] args) {
+		if(!Util.senderHasPermission("edit")) {
 			Message.sendDenied(args);
 			return;
 		}
 		
-		if(args.length == 1)
-		{
-			Help.getAuto();
+		if(args.length == 1) {
+			HelpCommand.getAuto();
 			return;
 		}
 		
-		if(args.length < 2 || args.length > 4)
-		{
+		if(args.length < 2 || args.length > 4) {
 			Message.sendInvalid(args);
 			return;
 		}
 
 		Mine curMine = CommandManager.getMine();
-		if(curMine == null)
-		{
+		if(curMine == null) {
 			Message.sendError(Language.getString("general.mine-not-selected"));
 			return;
 		}
 		
-		if(args[1].equalsIgnoreCase("toggle"))
-		{
-			if(curMine.)
-			{
-				Message.sendSuccess("'" + mineName + "' will no longer reset automatically.");
-				Regions.setBoolean(baseNode + ".reset", false);
+		if(args[1].equalsIgnoreCase("toggle")) {
+			if(args.length != 1) {
+				Message.sendError("Invalid parameters. Check your argument count!");
+				return;
 			}
-			else
-			{
-				Message.sendSuccess("'" + mineName + "' will now reset automatically.");
-				Regions.setBoolean(baseNode + ".reset", true);
+			
+			if(curMine.getAutomatic()) {
+				curMine.setAutomatic(false);
+				Message.sendSuccess("'" + curMine.getName() + "' will no longer reset automatically.");
 			}
-			Regions.saveData();
+			else {
+				curMine.setAutomatic(true);
+				Message.sendSuccess("'" + curMine.getName() + "' will now reset automatically.");
+			}
 			return;
 		}
-		else if(args[1].equalsIgnoreCase("time"))
-		{
+		else if(args[1].equalsIgnoreCase("time")) {
+			if(args.length != 2) {
+				Message.sendError("Invalid parameters. Check your argument count!");
+				return;
+			}
 			int time = Util.parseTime(args[2]);
-			if(time <= 0)
-			{
+			if(time <= 0) {
 				Message.sendError("Invalid time provided");
 				return;
 			}
-			Message.sendSuccess("'" + mineName + "' will now reset every " + time / 60 + " minute(s) " + time % 60 + " second(s)");
-			Regions.setInt(baseNode + ".reset-every", time);
-			Regions.saveData();
+			curMine.setResetPeriod(time);
+			Message.sendSuccess("'" + curMine.getName() + "' will now reset every " + time / 60 + " minute(s) " + time % 60 + " second(s)");
 			return;
 		}
-		else if(args[1].equalsIgnoreCase("warning"))
-		{
-			if(args[2].equalsIgnoreCase("toggle"))
-			{
-				if(Regions.getBoolean(baseNode + ".warn"))
+		else if(args[1].equalsIgnoreCase("warning")) {
+			if(args.length < 3) {
+				Message.sendError("Invalid parameters. Check your argument count!");
+				return;
+			}
+			
+			if(args[2].equalsIgnoreCase("toggle")) {
+				if(args.length != 3) {
+					Message.sendError("Invalid parameters. Check your argument count!");
+					return;
+				}
+				
+				if(curMine.getWarned())
 				{
-					Message.sendSuccess("Players will NOT be warned before '" + mineName + "' resets");
-					Regions.setBoolean(baseNode + ".warn", false);
+					curMine.setWarned(false);
+					Message.sendSuccess("Players will no longer be warned before '" + curMine.getName() + "' resets");
 				}
 				else
 				{
-					Message.sendSuccess("Players WILL be warned before '" + mineName + "' resets");
-					Regions.setBoolean(baseNode + ".warn", true);
+					curMine.setWarned(true);
+					Message.sendSuccess("Players will now be warned before '" + curMine.getName() + "' resets");
 				}
-				Regions.saveData();
 				return;
 			}
-			else if(args[2].equalsIgnoreCase("add") || args[2].equalsIgnoreCase("+"))
-			{
-				if(args.length != 4)
-				{
-					Message.sendInvalid(args);
+			else if(args[2].equalsIgnoreCase("add") || args[2].equalsIgnoreCase("+")) {
+				if(args.length != 4) {
+					Message.sendError("Invalid parameters. Check your argument count!");
 					return;
 				}
+				
 				int time = Util.parseTime(args[3]);
-				if(time <= 0)
-				{
+				if(time <= 0) {
 					Message.sendError("Invalid time provided");
 					return;
 				}
-				if(time > Regions.getInt(baseNode + ".reset-every"))
-				{
+				if(time > curMine.getResetPeriod()) {
 					Message.sendError("Time cannot be set to a value greater then the reset time");
 					return;
 				}
-				List<String> warnList = Regions.getList(baseNode + ".warn-times");
-				warnList.add(time + "");
-				Regions.setList(baseNode + ".warn-times", warnList);
-				Regions.saveData();
-				Message.sendSuccess(mineName + " will now send warnings " + time / 60 + " minute(s) " + time % 60 + " second(s) before the reset");
+				
+				List<Integer> warnList = curMine.getWarningTimes();
+				warnList.add(time);
+				curMine.setWarningTimes(warnList);
+				Message.sendSuccess(curMine.getName() + " will now send warnings " + time / 60 + " minute(s) " + time % 60 + " second(s) before the reset");
 				return;
 			}
-			else if(args[2].equalsIgnoreCase("remove") || args[2].equalsIgnoreCase("-"))
-			{
-
-				if(args.length != 4)
-				{
-					Message.sendInvalid(args);
+			else if(args[2].equalsIgnoreCase("remove") || args[2].equalsIgnoreCase("-")) {
+				if(args.length != 4) {
+					Message.sendError("Invalid parameters. Check your argument count!");
 					return;
 				}
+				
 				int time = Util.parseTime(args[3]);
-				if(time <= 0)
-				{
+				if(time <= 0) {
 					Message.sendError("Invalid time provided");
 					return;
 				}
-				List<String> warnList = Regions.getList(baseNode + ".warn-times");
-				int index = warnList.indexOf(time + "");
-				if(index == -1)
-				{
-					Message.sendError("'" + mineName + "' does not send a warning " + time + " minute(s) before the reset");
+				
+				List<Integer> warnList = curMine.getWarningTimes();
+				int index = warnList.indexOf(time);
+				if(index == -1) {
+					Message.sendError("'" + curMine.getName() + "' does not send a warning " + time + " minute(s) before the reset");
 					return;
 				}
+				
 				warnList.remove(index);
-				Regions.setList(baseNode + ".warn-times", warnList);
-				Regions.saveData();
-				Message.sendSuccess(mineName + " will no longer send a warning " + time + " minute(s) before the reset");
+				curMine.setWarningTimes(warnList);
+				Message.sendSuccess(curMine.getName() + " will no longer send a warning " + time + " minute(s) before the reset");
 				return;
 			}
 			else

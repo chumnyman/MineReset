cc127b2357cb177613c993b4c8c4156e76b1eeac
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-06-08 06:48:17 -0700

    Updated SelectCommand and the help command

--- src/com/wolvencraft/MineReset/cmd/HelpCommand.java
+++ src/com/wolvencraft/MineReset/cmd/HelpCommand.java
@@ -7,36 +7,33 @@ import com.wolvencraft.MineReset.CommandManager;
 import com.wolvencraft.MineReset.config.Language;
 import com.wolvencraft.MineReset.util.Util;
 
-public class HelpCommand
-{	
-	public static void getAuto()
-	{
+public class HelpCommand {	
+	public static void getAuto() {
 		formatHeader(20, "Timer");
 		formatHelp("auto", "toggle", "Toggles the automatic resets on and off", "");
 		formatHelp("auto", "time <time>", "Changes the automatic reset time to the value specified", "");
+		formatHelp("auto", "warning toggle", "Toggles reset warnings on and off", "");
 		formatHelp("auto", "warning add <time>", "Adds a warning at time specified", "");
 		formatHelp("auto", "warning remove <time>", "Adds a warning at time specified", "");
-		formatHelp("auto", "warning clear", "Removes all warnings from the mine", "");
+		return;
 	}
 	
-	public static void getBlacklist()
-	{
+	public static void getBlacklist() {
 		formatHeader(20, "Blacklist");
 		formatHelp("blacklist", "toggle", "Enables the use of blacklist for the mine", "");
 		formatHelp("blacklist", "whitelist", "Makes the blacklist be treated as a whitelist", "");
 		formatHelp("blacklist", "add <block>", "Adds a block to the blacklist", "");
 		formatHelp("blacklist", "remove <block>", "Removes a block from the whitelist", "");
+		return;
 	}
 	
-	public static void getConfig()
-	{
+	public static void getConfig() {
 		formatHeader(20, "Configuration");
 		formatHelp("config", "save", "Saves the data into a configuration file", "");
 		formatHelp("config", "load", "Loads the configuration from a file", "");
 	}
 	
-	public static void getEdit()
-	{
+	public static void getEdit() {
 		formatHeader(20, "Editing");
 		formatHelp("edit", "<name>", "Selects a mine to edit its properties", "");
 		formatHelp("none", "", "De-selects the mine", "");
@@ -46,10 +43,10 @@ public class HelpCommand
 		formatHelp("remove", "<block> [persentage]", "Removes the specified persentage of a block from the mine", "");
 		formatNote("If no persentage is provided, the block will be removed completely");
 		formatHelp("delete", "", "Completely deletes all the data about the selected mine", "");
+		return;
 	}
 	
-	public static void getHelp()
-	{
+	public static void getHelp() {
 		String title = Language.getString("general.title");
 		formatHeader(20, title);
 		
@@ -63,39 +60,32 @@ public class HelpCommand
 		formatHelp("delete", "<name>", "Completely deletes a mine", "edit");
 		formatHelp("auto", "", "Shows a help page on how to automate the mine", "edit");
 		formatHelp("protection", "", "Shows how to set up a mine protection", "edit");
-		
 		return;
 	}
 	
-	public static void getInfo()
-	{
+	public static void getInfo() {
 		formatHeader(20, "Information");
 		formatHelp("info", "<name>", "Returns the information about a mine", "info");
 		formatHelp("time", "<name>", "Returns the next reset time for the mine", "time");
 		return;
 	}
 	
-	public static void getProtection()
-	{
+	public static void getProtection() {
 		formatHeader(20, "Protection");
 		formatHelp("prot", "pvp", "Toggles the PVP on and off for the current mine", "");
-		
 		formatHelp("prot", "break toggle", "Enables or disables the block breaking protection", "");
 		formatHelp("prot", "break blacklist toggle", "Enables or disables the block breaking blacklist", "");
 		formatHelp("prot", "break blacklist whitelist", "Should the blacklist be treated as a whitelist?", "");
 		formatHelp("prot", "break blacklist add", "Add a block to the block breaking blacklist", "");
 		formatHelp("prot", "break blacklist remove", "Remove a block from the block breaking blacklist", "");
-
 		formatHelp("prot", "place blacklist toggle", "Enables or disables the block breaking blacklist", "");
 		formatHelp("prot", "place blacklist whitelist", "Should the blacklist be treated as a whitelist?", "");
 		formatHelp("prot", "place blacklist add", "Add a block to the block breaking blacklist", "");
 		formatHelp("prot", "place blacklist remove", "Remove a block from the block breaking blacklist", "");
-		
 		return;
 	}
 	
-	public static void getReset()
-	{
+	public static void getReset() {
 		formatHeader(20, "Reset");
 		formatHelp("reset", "<name> [generator]", "Resets the mine manually", "");
 		formatNote("Resets the mine according to the generator defined by the configuration.");
@@ -103,16 +93,13 @@ public class HelpCommand
 		return;
 	}
 	
-	public static void getSave()
-	{
+	public static void getSave() {
 		formatHeader(20, "Save");
 		formatHelp("save", "<name>", "Saves the region for future use", "");
-		
 		return;
 	}
 	
-	public static void getSelect()
-	{
+	public static void getSelect() {
 		formatHeader(20, "Selecting");
 		formatHelp("hpos1", "", "Creates a reference point 1 at the block you are looking at", "");
 		formatHelp("hpos2", "", "Creates a reference point 2 at the block you are looking at", "");
@@ -120,50 +107,42 @@ public class HelpCommand
 		formatHelp("pos1", "", "Creates a reference point 1 at your immediate location", "");
 		formatHelp("pos2", "", "Creates a reference point 2 at your immediate location", "");
 		formatNote("You can also select a region with your normal World Edit tool");
-		
 		return;
 	}
 	
-	public static void getSign()
-	{
+	public static void getSign() {
 		formatHeader(20, "Signs");
 		formatHelp("sign", "create", "Saves the targeted sign into a file", "");
 		formatHelp("sign", "parent <id>", "Sets the sign's parent to the one specified", "");
 		formatHelp("sign", "reset", "Marks the targeted sign as mine-resetting", "");
 		formatHelp("sign", "remove", "Removes the targeted sign from data", "");
-		
 		return;
 	}
 	
-	public static void getTeleport()
-	{
+	public static void getTeleport() {
 		formatHeader(20, "Teleportation");
 		formatHelp("warp", "<name>", "Teleports you to the mine warp location", "");
 		formatHelp("warp", "set", "Sets a warp for the current mine", "");
 		return;
 	}
 	
-	private static void formatHelp(String command, String arguments, String description, String node)
-	{
+	private static void formatHelp(String command, String arguments, String description, String node) {
 		CommandSender sender = CommandManager.getSender();
 		if(!arguments.equalsIgnoreCase("")) arguments = " " + arguments;
 		if(Util.senderHasPermission(node))
-			sender.sendMessage(ChatColor.BLUE + "/mine " + command + ChatColor.GRAY + arguments + ChatColor.WHITE + " " + description);
-		
+			sender.sendMessage(ChatColor.GOLD + "/mine " + command + ChatColor.GRAY + arguments + ChatColor.WHITE + " " + description);
 		return;
 	}
 	
-	private static void formatHeader(int padding, String name)
-	{
+	private static void formatHeader(int padding, String name) {
 		CommandSender sender = CommandManager.getSender();
 		String spaces = "";
 		for(int i = 0; i < padding; i++)
 			spaces = spaces + " ";
-		sender.sendMessage(spaces + "-=[ " + ChatColor.DARK_AQUA + name + ChatColor.WHITE + " ]=-");
+		sender.sendMessage(spaces + "-=[ " + ChatColor.BLUE + name + ChatColor.WHITE + " ]=-");
 	}
 	
-	private static void formatNote(String message)
-	{
+	private static void formatNote(String message) {
 		CommandSender sender = CommandManager.getSender();
 		sender.sendMessage(" " + message);
 	}
--- src/com/wolvencraft/MineReset/cmd/SelectCommand.java
+++ src/com/wolvencraft/MineReset/cmd/SelectCommand.java
@@ -1,5 +1,6 @@
 package com.wolvencraft.MineReset.cmd;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -11,54 +12,65 @@ public class SelectCommand
 {
 	public static void run(String[] args)
 	{
-		if(!Util.isPlayer() || !Util.senderHasPermission("edit"))
-		{
+		Player player;
+		if(CommandManager.getSender() instanceof Player) {
+			player = (Player) CommandManager.getSender();
+		}
+		else {
+			Message.sendError("This command cannot be executed via console");
+			return;
+		}
+		if(!Util.isPlayer() || !Util.senderHasPermission("edit")) {
 			Message.sendDenied(args);
 			return;
 		}
 		
-		if(args.length == 1)
-		{
+		if(args.length == 1) {
 			HelpCommand.getSelect();
 			return;
 		}
-		if(args.length > 2)
-		{
-			Message.sendInvalid(args);
+		if(args.length > 2) {
+			Message.sendError("Invalid parameters. Check your argument count!");
 			return;
 		}
 		
 		Location loc = null;
-		Player player = (Player) CommandManager.getSender();
 		
-		if(args[1].equalsIgnoreCase("hpos1"))
-		{
+		if(args[1].equalsIgnoreCase("hpos1")) {
 			loc = player.getTargetBlock(null, 100).getLocation();
-			Message.sendSuccess ("First point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
 			CommandManager.setLocation(loc, 0);
+			String message = "First point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")";
+			if(Util.locationsSet()) message = message + ": " + Util.getBlockCount() + " blocks";
+			Message.sendSuccess (message);
 		}
-		else if(args[1].equalsIgnoreCase("pos1"))
-		{
+		else if(args[1].equalsIgnoreCase("pos1")) {
 			loc = player.getLocation();
-			Message.sendSuccess ("First point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
 			CommandManager.setLocation(loc, 0);
+			String message = "First point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")";
+			if(Util.locationsSet()) message = message + ": " + Util.getBlockCount() + " blocks";
+			Message.sendSuccess (message);
 		}
-		else if(args[1].equalsIgnoreCase("hpos2"))
-		{
+		else if(args[1].equalsIgnoreCase("hpos2")) {
 			loc = player.getTargetBlock(null, 100).getLocation();
-			Message.sendSuccess ("Second point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
 			CommandManager.setLocation(loc, 1);
+			String message = "Second point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")";
+			if(Util.locationsSet()) message = message + ": " + Util.getBlockCount() + " blocks";
+			Message.sendSuccess (message);
 		}
-		else if(args[1].equalsIgnoreCase("pos2"))
-		{
+		else if(args[1].equalsIgnoreCase("pos2")) {
 			loc = player.getLocation();
-			Message.sendSuccess ("Second point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
 			CommandManager.setLocation(loc, 1);
+			String message = "Second point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")";
+			if(Util.locationsSet()) message = message + ": " + Util.getBlockCount() + " blocks";
+			Message.sendSuccess (message);
 		}
-		else
-		{
+		else {
 			Message.sendInvalid(args);
 			return;
+		}
+		
+		if(Util.locationsSet()) {
+			Message.sendSuccess("Both reference points are set! You can now save the region with " + ChatColor.GOLD + "/mine save");
 		}
 		
 		return;

ef433703f8c845ce41c7c1942047dc1132a6a1c6
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-06-07 17:18:23 -0700

    Convert MineUtil to the new format

--- src/com/wolvencraft/MineReset/util/MineUtils.java
+++ src/com/wolvencraft/MineReset/util/MineUtils.java
@@ -3,119 +3,73 @@ package com.wolvencraft.MineReset.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-import com.wolvencraft.MineReset.config.Regions;
+import com.wolvencraft.MineReset.MineReset;
+import com.wolvencraft.MineReset.mine.Mine;
+import com.wolvencraft.MineReset.mine.MineBlock;
 
 public class MineUtils
 {
 	/**
-	 * Checks if the mine exists
-	 * @param name Name of the mine being checked
+	 * Returns the object of the mine with the given id
+	 * @param id Name of the mine being checked
 	 * @return True if the mine exists, False if it does not
 	 */
-	public static boolean exists(String name)
+	public static Mine getMine(String id)
 	{
-		List<String> mineList = Regions.getList("data.list-of-mines");
-		if(mineList.indexOf(name) == -1) return false;
-		else return true;
+		for(Mine curMine : MineReset.getMines())
+		{
+			if(curMine.getName().equalsIgnoreCase(id)) return curMine;
+		}
+		return null;
 	}
 	
-	
-	/**
-	 * Determines if a specific player is in the mine
-	 * @param player Player
-	 * @param mineName Name of the mine
-	 * @return true if a player is in the mine, false if he is not
-	 */
-	public static boolean playerInTheMine(Player player, String mineName) {
-        return isLocationInMine(player.getLocation(), mineName);
-	}
-
-    public static boolean isBlockInMine(Block block, String mine) {
-        return isLocationInMine(block.getLocation(), mine);
-    }
-
-    /**
-     * Slightly more abstract method to determine if a mine contains a location or not
-     * @param location location object
-     * @param mine mine name
-     * @return true if location is in mine
-     */
-    public static boolean isLocationInMine(Location location, String mine) {
-        int[] x = {Regions.getInt("mines." + mine + ".coordinates.pos0.x"), Regions.getInt("mines." + mine + ".coordinates.pos1.x")};
-        int[] y = {Regions.getInt("mines." + mine + ".coordinates.pos0.y"), Regions.getInt("mines." + mine + ".coordinates.pos1.y")};
-        int[] z = {Regions.getInt("mines." + mine + ".coordinates.pos0.z"), Regions.getInt("mines." + mine + ".coordinates.pos1.z")};
-
-        return (location.getX() >= x[0] && location.getX() <= x[1])     //if x matches
-                && (location.getY() >= y[0] && location.getY() <= y[1]) //and y
-                && (location.getZ() >= z[0] && location.getZ() <= z[1]);//and z
-    }
 	/**
 	 * Teleports a player to the mine specified
 	 * @param player Player to be teleported
-	 * @param mineName The name of the mine
+	 * @param curMine The mine to be teleported to
 	 */
-	public static void warpToMine(Player player, String mineName)
-	{
-		String newLocWorld = Regions.getString("mines." + mineName + ".coordinates.world");
-		double[] coords = {
-				Regions.getDouble("mines." + mineName + ".coordinates.pos2.x"),
-				Regions.getDouble("mines." + mineName + ".coordinates.pos2.y"),
-				Regions.getDouble("mines." + mineName + ".coordinates.pos2.z"),
-				Regions.getDouble("mines." + mineName + ".coordinates.pos2.yaw"),
-				Regions.getDouble("mines." + mineName + ".coordinates.pos2.pitch")
-		};
-		Location newLoc = new Location(Bukkit.getServer().getWorld(newLocWorld), coords[0], coords[1], coords[2], (float)coords[3], (float)coords[4]);
-		
-		player.teleport(newLoc);
+	public static void warpToMine(Player player, Mine curMine) {
+		player.teleport(curMine.getWarp());
 	}
 	
-	public static List<String> getSortedList(String mineName)
+	public static List<String> getSortedList(Mine curMine)
 	{
-		List<String> itemList = Regions.getList("mines." + mineName + ".materials.blocks");
-		List<String> weightList = Regions.getList("mines." + mineName + ".materials.weights");
-		List<String> finalList = new ArrayList<String>(itemList.size());
+		List<MineBlock> blocks = curMine.getBlocks();
+		List<String> finalList = new ArrayList<String>(blocks.size());
 		
-		String tempItem = null, tempWeight = null;
-		for(int j = weightList.size() - 1; j > 0 ; j--)
+		MineBlock tempBlock;
+		for(int j = blocks.size(); j > 0; j--)
 		{
 			for(int i = 0; i < j; i++)
 			{
-				if(Double.parseDouble(weightList.get(i + 1)) > Double.parseDouble(weightList.get(i)))
+				if(blocks.get(i + 1).getChance() > blocks.get(i).getChance())
 				{
-					tempItem = itemList.get(i);
-					tempWeight = weightList.get(i);
-	
-					itemList.set(i, itemList.get(i + 1));
-					weightList.set(i, weightList.get(i + 1));
-	
-					itemList.set(i + 1, tempItem);
-					weightList.set(i + 1, tempWeight);
+					tempBlock = blocks.get(i).clone();
+					blocks.set(i, blocks.get(i + 1).clone());
+					blocks.set(i + 1, tempBlock.clone());
 				}
 				
 			}
 		}
 		
-		for(int i = 0; i < itemList.size(); i++)
+		for(MineBlock block : blocks)
 		{
-			String blockName = Material.getMaterial(Integer.parseInt(itemList.get(i).split(":")[0])).toString().toLowerCase().replace("_", " ");
-			if(!itemList.get(i).split(":")[1].equalsIgnoreCase("0"))
+			String blockName = block.getBlock().getItemType().toString().toLowerCase().replace("_", " ");
+			if(block.getBlock().getData() != 0)
 			{
-				blockName = Util.parseMetadata(itemList.get(i).split(":"), true) + " " + blockName;
+				String[] tempBlockName = {block.getBlock().getItemTypeId() + "", block.getBlock().getData() + ""};
+				blockName = Util.parseMetadata(tempBlockName, true) + " " + blockName;
 			}
 			String blockWeight = null;
-			if(Double.parseDouble(weightList.get(i)) < 10)
-				blockWeight = " " + weightList.get(i) + "%";
+			if((block.getChance() * 100) < 10)
+				blockWeight = " " + (block.getChance() * 100) + "%";
 			else
-				blockWeight = weightList.get(i) + "%";
+				blockWeight = (block.getChance() * 100) + "%";
 			
-			if(Double.parseDouble(weightList.get(i)) != 0)
+			if(block.getChance() != 0)
 				finalList.add(" - " + blockWeight + " " + ChatColor.GREEN + blockName);
 		}
 		
@@ -126,27 +80,27 @@ public class MineUtils
 	
 	/**
 	 * Returns the time period at which a certain mine is reset
-	 * @param mineName Mine to check
+	 * @param curMine Mine to check
 	 * @return Time period at which the mine is reset
 	 */
-	public static int getResetTime(String mineName)
+	public static int getResetTime(Mine curMine)
 	{
-		if(Regions.getString("mines." + mineName + ".parent") == null)
-			return Regions.getInt("mines." + mineName + ".reset.auto.reset-every");
+		if(curMine.getParent() == null)
+			return curMine.getResetPeriod();
 		else
-			return getNextReset(Regions.getString("mines." + mineName + ".parent"));
+			return getResetTime(curMine.getParent());
 	}
 	
 	/**
 	 * Returns the time in which a certain mine is reset
-	 * @param mineName Mine to check
+	 * @param curMine Mine to check
 	 * @return Seconds until the next reset
 	 */
-	public static int getNextReset(String mineName)
+	public static int getNextReset(Mine curMine)
 	{
-		if(Regions.getString("mines." + mineName + ".parent") == null)
-			return Regions.getInt("mines." + mineName + ".reset.auto.data.next");
+		if(curMine.getParent() == null)
+			return ((int)curMine.getNextAutomaticResetTick()) * 20;
 		else
-			return getNextReset(Regions.getString("mines." + mineName + ".parent"));
+			return getNextReset(curMine.getParent());
 	}
 }

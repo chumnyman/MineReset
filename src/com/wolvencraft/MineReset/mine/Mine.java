8cece5f7a956b522458bba1aa02939cf495c5396
Jeremy Koletar <jjkoletar@gmail.com>
2012-06-12 15:01:41 -0500

    Holy infinite recursion batman!

--- src/com/wolvencraft/MineReset/mine/Mine.java
+++ src/com/wolvencraft/MineReset/mine/Mine.java
@@ -387,4 +387,8 @@ public class Mine implements ConfigurationSerializable, Listener {
     public Blacklist getPlaceBlacklist() {
     	return placeBlacklist;
     }
+
+    public boolean hasParent() {
+        return parent != null;
+    }
 }

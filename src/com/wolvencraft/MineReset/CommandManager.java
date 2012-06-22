1bb170a988a1326b7f16c015e8cbe8336ab12b1a
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-06-22 12:53:55 -0700

    Bug fix: Cooldown command not registred with CommandManager

--- src/com/wolvencraft/MineReset/CommandManager.java
+++ src/com/wolvencraft/MineReset/CommandManager.java
@@ -35,7 +35,7 @@ public class CommandManager implements CommandExecutor
 			BlacklistCommand.run(args);
 		else if(args[0].equalsIgnoreCase("configuration") || args[0].equalsIgnoreCase("config"))
 			ConfigurationCommand.run(args);
-		else if(args[0].equalsIgnoreCase("edit") || args[0].equalsIgnoreCase("none") || args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("+") || args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("-") || args[0].equalsIgnoreCase("delete") || args[0].equalsIgnoreCase("del") || args[0].equalsIgnoreCase("name") || args[0].equalsIgnoreCase("silent") || args[0].equalsIgnoreCase("generator") || args[0].equalsIgnoreCase("link"))
+		else if(args[0].equalsIgnoreCase("edit") || args[0].equalsIgnoreCase("none") || args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("+") || args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("-") || args[0].equalsIgnoreCase("delete") || args[0].equalsIgnoreCase("del") || args[0].equalsIgnoreCase("name") || args[0].equalsIgnoreCase("silent") || args[0].equalsIgnoreCase("generator") || args[0].equalsIgnoreCase("link") || args[0].equalsIgnoreCase("cooldown"))
 			EditCommand.run(args);
 		else if(args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("?"))
 			HelpCommand.getHelp();
